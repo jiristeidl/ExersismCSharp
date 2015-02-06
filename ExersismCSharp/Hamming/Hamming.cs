d5b4b454ea209e5df43f9bf831f8d894bb37313f
jiristeidl <jiristeidl+github@gmail.com>
2015-02-06 12:55:12 +0100

    updated exercises and First hamming iteration

---
+++ ExersismCSharp/Hamming/Hamming.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExersismCSharp.Hamming
+{
+    class Hamming
+    {
+        internal static int Compute(string originalStrand, string newStrand)
+        {
+            int differences = 0;
+            if (!originalStrand.Equals(newStrand))
+            {
+                for (int i = 0; i < originalStrand.Length; i++)
+                {
+                    if (!originalStrand[i].Equals(newStrand[i]))
+                    {
+                        differences++;
+                    }
+                }
+            }
+            return differences;
+        }
+    }
+}
---
+++ exercises/csharp/anagram/Anagram.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ExersismCSharp.anagram
+{
+    internal class Anagram
+    {
+        private string orignalWord;
+
+        public Anagram(string originalWord)
+        {
+            this.orignalWord = originalWord;
+        }
+
+        internal string[] Match(string[] anagramCandidates)
+        {
+            var temporaryResults = new List<string>();
+            foreach (var potentialAnagram in anagramCandidates)
+            {
+                if (IsAnagram(orignalWord, potentialAnagram) && !potentialAnagram.ToLower().Equals(orignalWord.ToLower()))
+                {
+                    temporaryResults.Add(potentialAnagram);
+                }
+            }
+
+            return temporaryResults.ToArray();
+        }
+
+        private bool ContainsMultipleRepetions(char[] charArray, char letter)
+        {
+            return charArray.Count(c => c.Equals(letter)) > 1;
+        }
+
+        private bool ContaisChar(char[] charArray, char letter)
+        {
+            return charArray.Contains(letter);
+        }
+
+        private bool IsAnagram(string originalWord, string potentialAnagram)
+        {
+            var originalCharArray = orignalWord.ToLower().ToCharArray();
+            var potentialAnagramCharArray = potentialAnagram.ToLower().ToCharArray();
+
+            if (originalCharArray.Length == potentialAnagramCharArray.Length)
+            {
+                var matchMatrix = new bool[originalCharArray.Length];
+
+                for (int i = 0; i < originalCharArray.Length; i++)
+                {
+                    if (ContaisChar(potentialAnagramCharArray, originalCharArray[i]))
+                    {
+                        matchMatrix[i] = isMatch(originalCharArray, potentialAnagramCharArray, originalCharArray[i]);
+                    }
+                }
+                if (matchMatrix.All(v => v == true))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool isMatch(char[] originalCharArray, char[] potentialAnagramCharArray, char matchedChar)
+        {
+            int CharRepetitions = originalCharArray.Count(l => l.Equals(matchedChar));
+            if (CharRepetitions > 1 && potentialAnagramCharArray.Count(c => c.Equals(matchedChar)) == CharRepetitions)
+            {
+                return true;
+            }
+            else if (CharRepetitions == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
